c21db5bc8b062a68ff6b7f260820902a06dc1331
俞俞 <hanabiranoshitaniumore@gmail.com>
2017-10-17 15:05:41 +0800

    新增授權聲明 : CC BY-SA 4.0

--- Algorithms.Solution/Homework/Class 1/TransportObject.cs
+++ Algorithms.Solution/Homework/Class 1/TransportObject.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Author: Viyrex(aka Yuyu)
+// Contact: mailto:viyrex.aka.yuyu@gmail.com
+// Github: https://github.com/0x0001F36D
+// License: CC BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/
+
 namespace Algorithms.Solution.Homework.Class_1
 {
     using System;
@@ -8,10 +12,7 @@ namespace Algorithms.Solution.Homework.Class_1
     /// </summary>
     public class TransportObject : IEquatable<TransportObject>
     {
-        public static bool operator ==(TransportObject o1, TransportObject o2)
-            => o1?.Equals(o2) ?? false;
-        public static bool operator !=(TransportObject o1, TransportObject o2)
-            => !(o1?.Equals(o2) ?? false);
+        #region Public Constructors
 
         /// <summary>
         /// 初始化 <see cref="TransportObject"/> 類別的新執行個體
@@ -24,19 +25,29 @@ namespace Algorithms.Solution.Homework.Class_1
             this.Avoid = avoid;
         }
 
-        /// <summary>
-        /// 名稱
-        /// </summary>
-        public string Name { get; }
+        #endregion Public Constructors
 
+        #region Public Properties
 
         /// <summary>
         /// 要避開的 <see cref="TransportObject"/> 物件
         /// </summary>
         public TransportObject Avoid { get; }
 
-        public override int GetHashCode()
-            => this.Name.GetHashCode();
+        /// <summary>
+        /// 名稱
+        /// </summary>
+        public string Name { get; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public static bool operator !=(TransportObject o1, TransportObject o2)
+            => !(o1?.Equals(o2) ?? false);
+
+        public static bool operator ==(TransportObject o1, TransportObject o2)
+                                            => o1?.Equals(o2) ?? false;
 
         public bool Equals(TransportObject to)
             => to.GetHashCode() == this.GetHashCode();
@@ -44,8 +55,12 @@ namespace Algorithms.Solution.Homework.Class_1
         public override bool Equals(object obj)
             => (obj is TransportObject to) ? this.Equals(to) : false;
 
+        public override int GetHashCode()
+                            => this.Name.GetHashCode();
+
         public override string ToString()
             => this.Name;
 
+        #endregion Public Methods
     }
 }
